0138f762cc8328ea30d11be677d379fe5d46ad45
Mike <michael777gutty@gmail.com>
2025-05-16 17:14:30 -0400

    fix: fixing orders

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,7 +1,9 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Inject } from '@nestjs/common';
-import { CreateOrderDto } from './dto';
+import { Controller, Get, Post, Body, Param, Inject, ParseUUIDPipe, Query, Patch } from '@nestjs/common';
+import { CreateOrderDto, OrderPaginationDto, StatusDto } from './dto';
 import { ORDER_SERVICE } from 'src/config';
-import { ClientProxy } from '@nestjs/microservices';
+import { ClientProxy, RpcException } from '@nestjs/microservices';
+import { firstValueFrom } from 'rxjs';
+import { PaginationDto } from 'src/common';
 
 @Controller('orders')
 export class OrdersController {
@@ -15,12 +17,52 @@ export class OrdersController {
   }
 
   @Get()
-  findAll() {
-    return this.ordersClient.send('findAllOrders', {})
+  findAll(@Query() orderPaginationDto: OrderPaginationDto) {
+    return this.ordersClient.send('findAllOrders', orderPaginationDto)
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.ordersClient.send('findOneOrder', { id })
+  @Get('id/:id')
+  async findOne(@Param('id', ParseUUIDPipe) id: string) {
+    try {
+
+      const order = await firstValueFrom(
+        this.ordersClient.send('findOneOrder', { id })
+      )
+
+      return order;
+
+    } catch (error) {
+      throw new RpcException(error);
+    }
   }
+
+  @Get(':status')
+  async findAllByStatus(
+    @Param() statusDto: StatusDto,
+    @Query() paginationDto: PaginationDto,
+  ) {
+    try {
+
+      return this.ordersClient.send('findOneOrder', {
+        ...paginationDto,
+        status: statusDto.status
+      })
+
+    } catch (error) {
+      throw new RpcException(error);
+    }
+  }
+
+  @Patch(':id')
+  changeStatus(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() statusDto: StatusDto
+  ) {
+    try {
+      return this.ordersClient.send('changeOrderStatus', { id, status: statusDto.status })
+    } catch (error) {
+      throw new RpcException(error);
+    }
+  }
+
 }
