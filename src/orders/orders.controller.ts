da3323dd2b71ea6fd968e782db408131392a7264
Mike <michael777gutty@gmail.com>
2025-05-19 17:14:50 -0400

    fix: some fixes

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Get, Post, Body, Param, Inject, ParseUUIDPipe, Query, Patch } from '@nestjs/common';
 import { CreateOrderDto, OrderPaginationDto, StatusDto } from './dto';
-import { ORDER_SERVICE } from 'src/config';
+import { NATS_SERVICE } from 'src/config';
 import { ClientProxy, RpcException } from '@nestjs/microservices';
 import { firstValueFrom } from 'rxjs';
 import { PaginationDto } from 'src/common';
@@ -8,17 +8,17 @@ import { PaginationDto } from 'src/common';
 @Controller('orders')
 export class OrdersController {
   constructor(
-    @Inject(ORDER_SERVICE) private readonly ordersClient: ClientProxy
+    @Inject(NATS_SERVICE) private readonly client: ClientProxy
   ) { }
 
   @Post()
   create(@Body() createOrderDto: CreateOrderDto) {
-    return this.ordersClient.send('createOrder', createOrderDto);
+    return this.client.send('createOrder', createOrderDto);
   }
 
   @Get()
   findAll(@Query() orderPaginationDto: OrderPaginationDto) {
-    return this.ordersClient.send('findAllOrders', orderPaginationDto)
+    return this.client.send('findAllOrders', orderPaginationDto)
   }
 
   @Get('id/:id')
@@ -26,7 +26,7 @@ export class OrdersController {
     try {
 
       const order = await firstValueFrom(
-        this.ordersClient.send('findOneOrder', { id })
+        this.client.send('findOneOrder', { id })
       )
 
       return order;
@@ -43,7 +43,7 @@ export class OrdersController {
   ) {
     try {
 
-      return this.ordersClient.send('findOneOrder', {
+      return this.client.send('findOneOrder', {
         ...paginationDto,
         status: statusDto.status
       })
@@ -59,7 +59,7 @@ export class OrdersController {
     @Body() statusDto: StatusDto
   ) {
     try {
-      return this.ordersClient.send('changeOrderStatus', { id, status: statusDto.status })
+      return this.client.send('changeOrderStatus', { id, status: statusDto.status })
     } catch (error) {
       throw new RpcException(error);
     }
