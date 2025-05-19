8d3249706d48c13d638b44ba65d79dc40b911496
Mike <michael777gutty@gmail.com>
2025-05-19 16:10:43 -0400

    fix: adding nats

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -2,30 +2,30 @@ import { BadRequestException, Body, Controller, Delete, Get, Inject, Param, Pars
 import { ClientProxy, RpcException } from '@nestjs/microservices';
 import { catchError, firstValueFrom } from 'rxjs';
 import { PaginationDto } from 'src/common';
-import { PRODUCT_SERVICE } from 'src/config';
+import { NATS_SERVICE } from 'src/config';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 
 @Controller('products')
 export class ProductsController {
   constructor(
-    @Inject(PRODUCT_SERVICE) private readonly productClient: ClientProxy
+    @Inject(NATS_SERVICE) private readonly client: ClientProxy
   ) { }
 
   @Post()
   createProduct(@Body() createProductDto: CreateProductDto) {
-    return this.productClient.send({ cmd: 'create_product' }, createProductDto);
+    return this.client.send({ cmd: 'create_product' }, createProductDto);
   }
 
   @Get()
   findAllProducts(@Query() paginationDto: PaginationDto) {
-    return this.productClient.send({ cmd: 'find_all_products' }, paginationDto);
+    return this.client.send({ cmd: 'find_all_products' }, paginationDto);
   }
 
   @Get(':id')
   async findOne(@Param('id') id: string) {
 
-    return this.productClient.send({ cmd: 'find_one_product' }, { id })
+    return this.client.send({ cmd: 'find_one_product' }, { id })
       .pipe(
         catchError(err => { throw new RpcException(err) })
       )
@@ -44,7 +44,7 @@ export class ProductsController {
 
   @Delete(':id')
   deleteProduct(@Param('id') id: string) {
-    return this.productClient.send({ cmd: 'delete_product' }, { id }).pipe(
+    return this.client.send({ cmd: 'delete_product' }, { id }).pipe(
       catchError(err => { throw new RpcException(err) })
     );
   }
@@ -54,7 +54,7 @@ export class ProductsController {
     @Param('id', ParseIntPipe) id: number,
     @Body() updateProductDto: UpdateProductDto
   ) {
-    return this.productClient.send({ cmd: 'update_product' }, {
+    return this.client.send({ cmd: 'update_product' }, {
       id,
       ...updateProductDto
     }).pipe(
